78ed41b0c6653fa286fecd6151d4e0aa9ab33d74
arthid <r_tidz@hotmail.com>
2017-10-28 15:59:05 +0700

    finish 28oct2016

--- app/src/main/java/r_tidz/observice/fragment/RegisterFragment.java
+++ app/src/main/java/r_tidz/observice/fragment/RegisterFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import r_tidz.observice.MainActivity;
 import r_tidz.observice.R;
@@ -18,6 +19,9 @@ import r_tidz.observice.R;
 public class RegisterFragment extends Fragment{
 
 
+//    Explicit
+    private String nameString, userString, passwordString;
+
 
 
     @Override
@@ -27,9 +31,22 @@ public class RegisterFragment extends Fragment{
 //        create Toolbar
         createToolbar();
 
+//        Savecontroler
+
+        savecontroler();
+
 
     }   // Main method
 
+    private void savecontroler() {
+        ImageView imageView = getView().findViewById(R.id.imvsave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
 
 
